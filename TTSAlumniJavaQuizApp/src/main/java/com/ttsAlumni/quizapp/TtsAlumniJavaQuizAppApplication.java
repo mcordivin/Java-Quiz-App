45d91aedf3d7f4f5caeb038379c52ad379618fb0
Adrian Delpha <adrian.delpha@gmail.com>
2019-07-26 12:26:02 -0700

    Spring Boot Setup & Dependencies

---
+++ TTSAlumniJavaQuizApp/src/main/java/com/ttsAlumni/quizapp/TtsAlumniJavaQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.ttsAlumni.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtsAlumniJavaQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtsAlumniJavaQuizAppApplication.class, args);
+	}
+
+}
---
+++ TTSAlumniJavaQuizApp/src/test/java/com/ttsAlumni/quizapp/TtsAlumniJavaQuizAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ttsAlumni.quizapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TtsAlumniJavaQuizAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
